2bfab9e84a0140342ac5bcd4d22122401a8ea708
Sudhir <sudhirsingh3923@gmail.com>
2025-03-23 01:01:40 +0530

    made some changes in model

--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -1,22 +1,55 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     googleId: { type: String },
     verifyOtp: { type: String, default: "" },
     verifyOtpExpireAt: {type: Number, default: 0},
     isAccountVerified: { type: Boolean, default: false },
-    resetOtp: { type: String, default: "" },
-    resetOtpExpireAt: {type: Number, default: 0},
+    resetOtp: {
+      type: String,
+      default: "",
+    },
+    resetOtpExpireAt: {
+      type: Date,
+      default: null,
+    },
     authMethod: {
         type: String, 
         enum: ['local', 'google'],
         default: 'local'
     },
-    mobileNumber: { type: String, required: true }
-});
+    mobileNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    loginOtp: {
+      type: String,
+      default: "",
+    },
+    loginOtpExpireAt: {
+      type: Date,
+      default: null,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
